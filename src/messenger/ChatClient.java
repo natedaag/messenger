8845590a4c5bd9021e131cc6d61fdd8b9a532177
natedaag <natedaage@yahoo.com>
2017-11-06 12:39:15 -0700

    a complete re-write - got the bind exception to stop, but now the connection is refused.

--- src/messenger/ChatClient.java
+++ src/messenger/ChatClient.java
@@ -16,125 +16,40 @@ public class ChatClient extends JFrame implements Runnable {
 	
 	Socket socket;
 	JTextArea ta;
-	JButton send, logout;
-	JTextField tf;
 	
 	Thread thread;
 	
 	DataInputStream din;
 	DataOutputStream dout;
 	
-	String LoginName;
+	String loginName;
 	
 	ChatClient(String login) throws UnknownHostException, IOException{
 		super(login);
-		LoginName = login;
-		
-		addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e){
-				try {
-					dout.writeUTF(LoginName + " " + "LOGOUT");
-					System.exit(1);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
+		loginName = login;
 		
 		ta = new JTextArea(18, 50);
-		tf = new JTextField(50);
-		
-		tf.addKeyListener(new KeyListener(){
-
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO Auto-generated method stub
-				if(e.getKeyCode() == KeyEvent.VK_ENTER){
-					try {
-						if(tf.getText().length()>0)
-							dout.writeUTF(LoginName + " " + "DATA " + tf.getText().toString());
-						tf.setText("");
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
-		
-		send = new JButton("Send");
-		logout = new JButton("Logout");
-		
-		send.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				try {
-					if(tf.getText().length()>0)
-						dout.writeUTF(LoginName + " " + "DATA " + tf.getText().toString());
-					tf.setText("");
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-			
-		});
-		
-		logout.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				try {
-					dout.writeUTF(LoginName + " " + "LOGOUT");
-					System.exit(1);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-			
-		});
 		
 		socket = new Socket("localhost", 5217);
 		
 		din = new DataInputStream(socket.getInputStream());
-		dout = new DataOutputStream(socket.getOutputStream());
+		dout = new DataOutputStream(socket.getOutputStream());		
 		
-		dout.writeUTF(LoginName);
-		dout.writeUTF(LoginName + " " + "LOGIN");
+		dout.writeUTF(loginName);
+		dout.writeUTF(loginName + " " + "LOGIN");
 		
 		thread = new Thread(this);
 		thread.start();
 		setup();
 	}
-	
+
 	private void setup() {
-		// TODO Auto-generated method stub
-		setSize(600, 400);
+		setSize(500, 400);
 		
 		JPanel panel = new JPanel();
 		
 		panel.add(new JScrollPane(ta));
-		panel.add(tf);
-		panel.add(send);
-		panel.add(logout);
+		
 		add(panel);
 		
 		setVisible(true);
@@ -142,10 +57,9 @@ public class ChatClient extends JFrame implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		while(true){
+		while (true){
 			try {
-				ta.append("\n" + din.readUTF());
+				ta.append("/n" + din.readUTF());
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -153,5 +67,32 @@ public class ChatClient extends JFrame implements Runnable {
 		}
 	}
 	
+	public static void main(String[] args) throws UnknownHostException, IOException{
+		ChatClient client = new ChatClient("user1");
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/messenger/ChatServer.java
+++ src/messenger/ChatServer.java
@@ -7,13 +7,14 @@ import java.util.Vector;
 
 
 public class ChatServer {
-	static Vector ClientSockets;
-	static Vector LoginNames;
+	
+	static Vector clientSockets;
+	static Vector loginNames;
 	
 	ChatServer() throws IOException{
-		ServerSocket server = new ServerSocket(5217);
-		ClientSockets = new Vector();
-		LoginNames = new Vector();
+		ServerSocket server = new ServerSocket(0);
+		clientSockets = new Vector();
+		loginNames = new Vector();
 		
 		while(true){
 			Socket client = server.accept();
@@ -23,73 +24,66 @@ public class ChatServer {
 	
 	public static void main(String[] args) throws IOException{
 		ChatServer server = new ChatServer();
+		
 	}
 	
 	class AcceptClient extends Thread{
-		Socket ClientSocket;
+		Socket clientSocket;
 		DataInputStream din;
 		DataOutputStream dout;
 		AcceptClient(Socket client) throws IOException{
-			ClientSocket = client;
-			din = new DataInputStream(ClientSocket.getInputStream());
-			dout = new DataOutputStream(ClientSocket.getOutputStream());
+			din = new DataInputStream(clientSocket.getInputStream());
+			dout = new DataOutputStream(clientSocket.getOutputStream());
 			
-			String LoginName = din.readUTF();
+			String loginName = din.readUTF();
 			
-			LoginNames.add(LoginName);
-			ClientSockets.add(ClientSocket);
+			loginNames.add(loginName);
+			clientSockets.add(clientSocket);
 			
 			start();
 		}
-		
 		public void run(){
-			while(true){
+			while (true){
 				try {
 					String msgFromClient = din.readUTF();
 					StringTokenizer st = new StringTokenizer(msgFromClient);
-					String LoginName = st.nextToken();
-					String MsgType = st.nextToken();
-					int lo = -1;
-					String msg = "";
+					String loginName = st.nextToken();
+					String msgType = st.nextToken();
 					
-					while(st.hasMoreTokens()){
-						msg=msg+ " " + st.nextToken();
+					for(int i = 0; i < loginNames.size(); i++){
+						Socket pSocket = (Socket) clientSockets.elementAt(i);
+						DataOutputStream pOut = new DataOutputStream(pSocket.getOutputStream());
+						pOut.writeUTF(loginName +" has logged in.");
 					}
-					if(MsgType.equals("LOGIN")){
-						for(int i = 0; i<LoginNames.size(); i++){
-							Socket pSocket = (Socket) ClientSockets.elementAt(i);
-							DataOutputStream pOut = new DataOutputStream(pSocket.getOutputStream());
-							pOut.writeUTF(LoginName + " has logged in.");
-						}
-					}
-					else if(MsgType.equals("LOGOUT")){
-						for(int i = 0; i<LoginNames.size(); i++){
-							if(LoginName.equals(LoginNames.elementAt(i)))
-								lo = i;
-							Socket pSocket = (Socket) ClientSockets.elementAt(i);
-							DataOutputStream pOut = new DataOutputStream(pSocket.getOutputStream());
-							pOut.writeUTF(LoginName + " has logged out.");
-						}
-						if(lo >= 0){
-							LoginNames.removeElementAt(lo);
-							ClientSockets.removeElementAt(lo);
-						}
-					}
-					else {
-						for(int i = 0; i<LoginNames.size(); i++){
-							Socket pSocket = (Socket) ClientSockets.elementAt(i);
-							DataOutputStream pOut = new DataOutputStream(pSocket.getOutputStream());
-							pOut.writeUTF(LoginName + ": " + msg);
-						}
-					}
-					if(MsgType.equals("LOGOUT"))
-						break;
-					
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
